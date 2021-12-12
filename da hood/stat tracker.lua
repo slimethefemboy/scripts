79358ae8b809e6ec4fd1d7327a2c581594465865
Slimeeeee <84150401+slimethefemboy@users.noreply.github.com>
2021-12-12 01:46:11 -0500

    Create stat tracker.lua

---
+++ da hood/stat tracker.lua
@@ -0,0 +1,48 @@
+local web = ''
+
+if not syn.request then 
+ return;
+end
+
+repeat wait() until game:IsLoaded()
+repeat wait() until game.Players
+repeat wait() until game.Players.LocalPlayer
+repeat wait() until game.Players.LocalPlayer.Character
+
+
+  
+local plrs = game:GetService('Players')
+local lp = plrs.LocalPlayer
+local character = lp.Character
+local humanoid = character.Humanoid
+local pg = lp.PlayerGui
+local Main = pg.MainScreenGui
+local money = Main.MoneyText
+
+while true do
+local data = {
+    ["embeds"] = {
+        {
+        ["title"] = "da hood stats",
+        ["description"] = "User: " .. lp.Name .. '\n' .. 'money amount: ' .. money.text .. '\n' .. 'Player health: ' .. math.floor(humanoid.Health)  ,
+        ["type"] = "rich",
+        ["color"] = 0,
+        }
+    }
+}
+
+local req = syn.request({
+
+Url = web,
+
+Method = "POST",
+
+Headers = {
+["content-type"] = "application/json" 
+},
+
+Body = game:GetService("HttpService"):JSONEncode(data)
+    
+})
+wait(15)
+end
