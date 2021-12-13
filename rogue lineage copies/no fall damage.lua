778237be84030e4e0e27a19716072ebdba3b7b66
Slimeeeee <84150401+slimethefemboy@users.noreply.github.com>
2021-12-13 00:37:12 -0500

    Update no fall damage.lua

--- rogue lineage copies/no fall damage.lua
+++ rogue lineage copies/no fall damage.lua
@@ -1,7 +1,7 @@
 local funny
 funny = hookmetamethod(game,'__namecall', function(b,...)
-if not checkcaller() and getnamecallmethod() == 'FireServer' and tostring(b) == 'RemoteEvent' then
-return;
-end
-return funny(b,...)
+    if not checkcaller() and getnamecallmethod() == 'FireServer' and tostring(b) == 'RemoteEvent' then
+        return;
+    end
+    return funny(b,...)
 end)
