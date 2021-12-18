df743f70c1507d38629ec777513925eebdeb4fc2
Slimeeeee <84150401+slimethefemboy@users.noreply.github.com>
2021-12-17 20:23:43 -0500

    Update no fall damage.lua

--- [ALPHA] Blood Engine 2 : Slash Mania/no fall damage.lua
+++ [ALPHA] Blood Engine 2 : Slash Mania/no fall damage.lua
@@ -1,1 +1,6 @@
 game:GetService('Players').LocalPlayer.Character.FallDamage:Destroy()
+
+game:GetService('Players').LocalPlayer.CharacterAdded:Connect(function()
+wait(2)
+game:GetService('Players').LocalPlayer.Character.FallDamage:Destroy()
+end)
