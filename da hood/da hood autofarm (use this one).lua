6e3d8bd20dcd1db11c2724ec4433cd5fc3818562
Slimeeeee <84150401+slimethefemboy@users.noreply.github.com>
2021-12-29 18:33:24 -0500

    Delete da hood autofarm (use this one).lua

--- da hood/da hood autofarm (use this one).lua
+++
@@ -1,7 +0,0 @@
--- credits to charwar for the server hop https://v3rmillion.net/member.php?action=profile&uid=114844
-getgenv().Settings = {
-time_until_start = 20,
-speed = 4
-}
-
-loadstring(game:HttpGet('https://raw.githubusercontent.com/slimethefemboy/scripts/main/da%20hood/da%20hood%20autofarm%20dont%20use.lua'))()
