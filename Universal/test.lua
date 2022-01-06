12c365af741f997bbcb472611a05b95c499864f5
Slimeeeee <84150401+slimethefemboy@users.noreply.github.com>
2022-01-06 11:58:23 -0600

    Update test.lua

--- Universal/test.lua
+++ Universal/test.lua
@@ -1,21 +1,12 @@
--- stefanuk12 helped with __newindex hook
-local balls = {
-    WalkSpeed = 16,
-    JumpPower = 50
-}
-local hum = game:GetService('Players').LocalPlayer.Character.Humanoid
-local old
-old = hookmetamethod(game,'__index',function(a,b)
-    if not checkcaller() and a == hum and (b == 'Walkspeed' or b == 'JumpPower') then
-        return balls[b]
-    end
-    return old(a,b)
-end)
+local spoof = {
+  WalkSpeed = 16,
+  JumpPower = 50
+};
 
-local new
-new = hookmetamethod(game,'__newindex',function(a,b,c)
-    if not checkcaller() and a == hum and (b == 'WalkSpeed' or b == 'JumpPower') then
-        balls[b] = c; return
+local __index
+__index = hookmetamethod(game,'__index',function(a,b)
+    if not checkcaller() and a == hum and (tostring(b) == 'WalkSpeed' or tostring(b) == 'JumpPower') then
+        return spoof[b]
     end
-    return new(a,b,c)
+    return __index(a,b)
 end)
