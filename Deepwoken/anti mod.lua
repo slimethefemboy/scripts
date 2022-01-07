eba600de0d66ca18003bc88bcba6740ce3e35cb9
Slimeeeee <84150401+slimethefemboy@users.noreply.github.com>
2022-01-06 18:33:50 -0600

    Update and rename modlist.lua to anti mod.lua

--- Deepwoken/anti mod.lua
+++ Deepwoken/anti mod.lua
@@ -1,5 +1,7 @@
--- this contains a list of active mods and potential mods
+local plrs = game:GetService('Players')
+local lp = plrs.LocalPlayer
 local mods = {
+2998077478,
 151239872,
 167825083,
 17126105,
@@ -104,4 +106,23 @@ local mods = {
 139151151,
 168151412,
 708648
-}
+};
+
+-- kick function
+local kick = function(reason)
+    return lp:kick(reason)
+end
+
+-- check if mod is currently in game
+for i,v in pairs(plrs:GetChildren()) do 
+    if table.find(mods,v.UserId) then 
+        kick('potential mod found')
+    end
+end
+
+-- kick if mod joins game
+plrs.PlayerAdded:Connect(function(r)
+    if table.find(mods,r.UserId) then 
+        kick('potential mod joined')
+    end
+end)
