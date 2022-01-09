b4b42c3bcabc28d184aeabc8d600b35e3cf88c0a
Slimeeeee <84150401+slimethefemboy@users.noreply.github.com>
2022-01-08 21:13:29 -0500

    Update test.lua

--- Universal/test.lua
+++ Universal/test.lua
@@ -1,12 +1,1 @@
-local spoof = {
-  WalkSpeed = 16,
-  JumpPower = 50
-};
-
-local __index
-__index = hookmetamethod(game,'__index',function(a,b)
-    if not checkcaller() and a == hum and (tostring(b) == 'WalkSpeed' or tostring(b) == 'JumpPower') then
-        return spoof[b]
-    end
-    return __index(a,b)
-end)
+local retard = {}
